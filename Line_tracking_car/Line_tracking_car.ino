44e71c12ca8f973660c4c7582f533959300794cd
Shar Ali <sher67206@gmail.com>
2019-04-18 16:08:56 +0100

    Add obstacle detection

--- Line_tracking_car/Line_tracking_car.ino
+++ Line_tracking_car/Line_tracking_car.ino
@@ -1,6 +1,9 @@
 #define WHITE false
 #define BLACK true
-
+#include <Servo.h> //servo library
+Servo myservo; // create servo object to control servo
+int Echo = A4;  
+int Trig = A5; 
 int in1=6;
 int in2=7;
 int in3=8;
@@ -8,7 +11,7 @@ int in4=9;
 int ENA=5;
 int ENB=11;
 int ABS=100;
-
+int rightDistance = 0,leftDistance = 0,middleDistance = 0 ;
 void _mForward()
 {
   analogWrite(ENA,ABS);
@@ -57,12 +60,35 @@ void _mStop()
    digitalWrite(ENA,LOW);
    digitalWrite(ENB,LOW);
    Serial.println("Stop!");
-} 
+}
+ 
+/*Ultrasonic distance measurement Sub function*/
+int Distance_test()   
+{
+  digitalWrite(Trig, LOW);   
+  delayMicroseconds(2);
+  digitalWrite(Trig, HIGH);  
+  delayMicroseconds(20);
+  digitalWrite(Trig, LOW);   
+  float Fdistance = pulseIn(Echo, HIGH);  
+  Fdistance= Fdistance/58;       
+  return (int)Fdistance;
+}  
 
 void setup() 
 {
-  Serial.begin(9600);
-}
+  myservo.attach(3);// attach servo on pin 3 to servo object
+  Serial.begin(9600);     
+  pinMode(Echo, INPUT);    
+  pinMode(Trig, OUTPUT);  
+  pinMode(in1,OUTPUT);
+  pinMode(in2,OUTPUT);
+  pinMode(in3,OUTPUT);
+  pinMode(in4,OUTPUT);
+  pinMode(ENA,OUTPUT);
+  pinMode(ENB,OUTPUT);
+  _mStop();
+} 
 
 void loop() {
   int left_sensor,middle_sensor,right_sensor;
@@ -72,8 +98,19 @@ void loop() {
   Serial.println(left_sensor);
   Serial.println(middle_sensor);
   Serial.println(right_sensor);
-   if((left_sensor==WHITE)&&(right_sensor==BLACK)) 
-   { 
+  myservo.write(90);//setservo position according to scaled value
+  delay(500); 
+  middleDistance = Distance_test();
+  #ifdef send
+  Serial.print("middleDistance=");
+  Serial.println(middleDistance);
+  #endif
+  
+//  if (middleDistance<=20){
+//    _mStop();
+//  }else {
+  if((left_sensor==WHITE)&&(right_sensor==BLACK)) 
+  { 
     Serial.println("going right! RIGHT BLACK");
     _mright();          //The sensor detected that right car turn left immediately when it meets black line 
     delay(2);
@@ -113,5 +150,7 @@ void loop() {
     Serial.println("spinning!");
       _mleft(); 
       delay(2);
-  } 
+    } 
+  //}
+   
 }
